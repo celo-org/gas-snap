90274afbe45179f5de0a64126bb6cec4ba6fef02
Edwin Tops <enginebox2012@gmail.com>
2023-06-22 14:52:37 +0200

    Add celoTx io schema.

--- packages/snap/src/utils/types.ts
+++ packages/snap/src/utils/types.ts
@@ -1,17 +1,32 @@
 import * as t from 'io-ts'
 
-export const SimpleTransaction = t.intersection([
-    t.type({
-        to: t.string,
-        value: t.string
-    }),
-    t.partial({
-        feeCurrency: t.union([t.string, t.undefined])
-    })
+export const TransactionConfig = t.partial({
+    from: t.union([t.string, t.number]),
+    to: t.string,
+    value: t.union([t.number, t.string, t.bigint]),
+    gas: t.union([t.string, t.number]),
+    gasPrice: t.union([t.number, t.string, t.bigint]),
+    data: t.string,
+    nonce: t.number,
+    chainId: t.string,
+    common: t.string,
+    chain: t.string,
+    hardfork: t.string
+})
+
+export const CeloParams = t.partial({
+    feeCurrency: t.string,
+    gatewayFeeRecipient: t.string,
+    gatewayFee: t.string
+})
+
+export const CeloTx = t.intersection([
+    TransactionConfig,
+    CeloParams
 ])
 
 export const RequestParams = t.type({
-    tx:  SimpleTransaction
+    tx:  CeloTx
 })
 
 export type StableTokenBalance = {
